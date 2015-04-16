b94f5ca9b33579854f7d4617403e749047f666f6
radosz <radosz88@gmail.com>
2015-04-16 19:37:55 +0300

    Fix Syntax

--- Hero.py
+++ Hero.py
@@ -1,16 +1,23 @@
-# Hero class
 class Hero:
 
-    def __init__(self, hero_name, hero_title, health=100, mana=100, weapons, spells, mana_regeneration_rate=1):
+    def __init__(self, hero_name, weapons, spells, hero_title, health=100, mana=100, mana_regeneration_rate=1):
         self.__name = hero_name
         self.__title = hero_title
         self.__health = health
         self.__mana = mana
-        self__mana_regeneration = mana_regeneration_rate
+        self.__mana_regeneration_rate = mana_regeneration_rate
         # Each hero can have  a lot of weapons and spells, but can equip one
-        # weapon and one spell
+        # weapon and one spell class
         self.__spells = set(spells)
         self.__weapons = set(weapons)
+        self.__current_weapon = None
+        self.__current_spell = None
+
+    def get_current_weapon(self):
+        return self.__current_weapon
+
+    def get_current_spell(self):
+        return self.__current_spell
 
     def get_weapons(self):
         return self.__weapons
@@ -66,7 +73,7 @@ class Hero:
             self.__mana += self.__mana_regeneration_rate
             # Hero's mana cannot go above the start mana given to him,
             # neither he can go down below 0 mana
-            if self.is_mana_points_over_100p(regeneration_point):
+            if self.is_mana_points_over_100p(self.mana_regeneration_rate):
                 self.__mana = 100
         # valid if  is_move = True or is_move = False
         # always return mana
@@ -78,14 +85,14 @@ class Hero:
         self.__mana += mana_points
         return self.get_mana()
 
-    def equip(self, weapon, learn):
-        if weapon in get_weapons():
+    def equip(self, weapon):
+        if weapon in self.get_weapons():
             self.__current_weapon = weapon
         # In all other cases return Exception
         # TODO : Make equip exception ("This weapon not in hero inventory")
 
     def learn(self, spell):
-        if weapon in get_weapons():
+        if spell in self.get_spells():
             self.__current_spell = spell
         # TODO : Make learn exception ("This spell not in hero inventory")
 
